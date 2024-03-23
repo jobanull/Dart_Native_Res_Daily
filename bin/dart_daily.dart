0878e47e57560f7e7c51f8a08f5e0f0053da861b
Joban <jikhdan@gmail.com>
2024-03-23 19:08:12 +0700

    Starting Project

---
+++ bin/dart_daily.dart
@@ -0,0 +1,5 @@
+import 'package:dart_daily/dart_daily.dart' as daily;
+
+void main(List<String> args) {
+  print('Hello World: ${daily.tambah(2)}');
+}
---
+++ lib/dart_daily.dart
@@ -0,0 +1,3 @@
+int tambah(int a) {
+  return a + 2;
+}
